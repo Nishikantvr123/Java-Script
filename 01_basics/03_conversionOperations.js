fa9223eceeee9b2d6847181060b587299465771b
NISHI KANT VERMA <11339nishikantverma@gmail.com>
2025-04-30 17:26:12 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,19 @@
+let score = "33abc"
+let score1 = undefined
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+let valueInNumber1 = Number(score1)
+// console.log(typeof (valueInNumber));
+// console.log(valueInNumber)
+
+// console.log(valueInNumber1)
+
+// "33" => 33
+// "33abc"=> NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
