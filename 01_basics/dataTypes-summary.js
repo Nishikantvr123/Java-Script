cf13b688a659d781217f739926e686d44c7003cc
NISHI KANT VERMA <11339nishikantverma@gmail.com>
2025-05-14 17:59:35 +0000

    done

--- 01_basics/dataTypes-summary.js
+++ 01_basics/dataTypes-summary.js
@@ -6,5 +6,21 @@ const anotherId = Symbol('123')
 
 console.log(Id == anotherId) // false
 
+// const bigNumber = 34476346757334442232
+
 // Reference (Non primitive)
 // Array, Objects, Functions
+
+
+const heros = ["shaktiman", "iron man","hulk"];
+
+
+let myObj = {
+    name: "Nishikant",
+    age : 22
+}
+
+const myFunction= function(){
+    console.log("Hello world")
+}
+console.log(typeof myFunction);
